8f92901151c41a0abef2333c52a3342bb5b280e4
Gonçalo Oliveira <goncalo.oli@outlook.com>
2021-03-29 11:55:39 +0100

    Fixed message checking algorithm due to pkcs padding

---
+++ rsa_3_6.py
@@ -0,0 +1,8 @@
+prefix = "0001"
+hash = "d151c37125d9be7cde130ea3f0cc7f008f3f97b13b6aa438c87578e02ca957d5"
+A = "3031300d060960864801650304020105000420"
+total_len = 256
+pad_len = total_len - 1 - (len(A) + len(prefix) + len(hash)) // 2
+padded = prefix + "ff" * pad_len + "00" + A + hash
+
+print(padded)
